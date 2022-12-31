53e1396d945897834f6892ccc09d87713f3919e3
sky1122 <48091236+sky1122@users.noreply.github.com>
2022-12-30 20:27:13 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,9 @@ import HeaderWrap from './components/HeaderWrap';
 import MovieCardWrap from './components/MovieCardWrap';
 import InputFeild from './components/InputFeild';
 import DetailView from './components/DetailView';
-// the difference between function and arrow function in React
-// https://stackoverflow.com/questions/49306148/why-is-arrow-syntax-preferred-over-function-declaration-for-functional-react-com
 
 
-
-
-
-// funtional component
-// React.FC declear Function Component
-// React.ReactNode can be lots of different type
 const App: React.FC = () => {
-  // empty "" todo : string
-
-
 
   return (
 
